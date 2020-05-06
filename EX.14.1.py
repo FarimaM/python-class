97015aa38b6c268c24ca0ecb134db87a6cc5d98e
FarimaM <63778675+FarimaM@users.noreply.github.com>
2020-05-06 20:44:27 +0430

    Update and rename tamrine14.1.py to EX.14.1.py

--- EX.14.1.py
+++ EX.14.1.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+###1###
 a = [[1, 0, 1], [0, 1, 0], [1, 0, 1]]
 x= [-10, 10]
+
 t=np.choose(a, x)
 print(t)
+###2###
 y=np.power(x,3)
 print("baze",np.square(y))
-
-
-theta=np.angle([1.0, 1.0j, 1+1j])               # in radians
-z=np.angle(1+1j, deg=True)                  # in degrees
+###3####
+theta=np.angle([1.0, 1.0j, 1+1j])  # in radians
+z=np.angle(1+1j, deg=True)# in degrees
+###4###
 u1=np.sin(z)
 u2=np.cos(z)
+###5###
 meshPoints =np.linspace(-1,1,num=500)
 print(meshPoints)
-pi=3.14
-
+###6###
 print("53th value of meshPoints is {}".format(meshPoints[52]))
-plt.plot(meshPoints, np.sin(2 *pi * meshPoints))
+###7###
+plt.plot(meshPoints, np.sin(2 *np.pi * meshPoints))
 plt.show()
-
 plt.savefig("sin.png")
+
